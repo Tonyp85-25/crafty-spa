1351f73f9e796daa2e4f1cd784ade48f8d783671
Tonyp85-25 <anthony-paulin@etudiant.oclock.io>
2024-09-05 22:42:46 +0200

    chore: add continuous delivery with github actions

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Crafty SPA</h1>
+      <h1>Crafty SPA with continuous deployment</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
